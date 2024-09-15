da24e760c2b0a70c2c6e83236233b29ba82a950c
natuyukisakura <MoriRinMonro1@gmail.com>
2024-09-15 20:43:09 +0900

    [5-32] つぶやき機能の実装

--- components/Timeline.tsx
+++ components/Timeline.tsx
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import Post from "./Post";
+import apiClient from "@/lib/apiClient";
 
 const Timeline = () => {
+  const [postText, setPostText] = useState<string>("");
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      await apiClient.post("/posts/post", {
+        content: postText,
+      });
+      setPostText("");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div>
       <div className="min-h-screen bg-gray-100">
         <main className="container mx-auto py-4">
           <div className="bg-white shadow-md rounded p-4 mb-4">
-            <form>
+            <form onSubmit={handleSubmit}>
               <textarea
                 className="w-full h-24 p-2 border border-gray-300 rounded resize-none focus:outline-none focus:ring-2 focus:ring-blue-400"
                 placeholder="What's on your mind?"
+                onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) =>
+                  setPostText(e.target.value)
+                }
+                value={postText} // これを忘れてると入力した内容が消えない
               ></textarea>
               <button
                 type="submit"
