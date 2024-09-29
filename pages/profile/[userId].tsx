e4b25c533bc5da8932e3267a0c9a32aee9145e76
natuyukisakura <MoriRinMonro1@gmail.com>
2024-09-29 18:20:41 +0900

    [7-56] プロフィール画面の表示

--- pages/profile/[userId].tsx
+++ pages/profile/[userId].tsx
@@ -1,16 +1,46 @@
 // 動的ルーティング
 
-const UserProfile = () => {
+import apiClient from "@/lib/apiClient";
+import { Profile } from "@/types";
+
+type Props = {
+  profile: Profile;
+};
+
+export const getServerSideProps = async (context: any) => {
+  const { userId } = context.query;
+  try {
+    const profileResponse = await apiClient.get(`/users/profile/${userId}`);
+
+    return {
+      props: {
+        profile: profileResponse.data,
+      },
+    };
+  } catch (e) {
+    return {
+      notFound: true,
+    };
+  }
+};
+
+const UserProfile = ({ profile }: Props) => {
   return (
     <div>
       <div className="container mx-auto px-4 py-8">
         <div className="w-full max-w-xl mx-auto">
           <div className="bg-white shadow-md rounded-lg p-6 mb-4">
             <div className="flex items-center">
-              <img className="w-20 h-20 rounded-full mr-4" alt="User Avatar" />
+              <img
+                className="w-20 h-20 rounded-full mr-4"
+                alt="User Avatar"
+                src={profile.profileImageUrl}
+              />
               <div>
-                <h2 className="text-2xl font-semibold mb-1">shincode</h2>
-                <p className="text-gray-600">はじめまして</p>
+                <h2 className="text-2xl font-semibold mb-1">
+                  {profile.user.username}
+                </h2>
+                <p className="text-gray-600">{profile.bio}</p>
               </div>
             </div>
           </div>
