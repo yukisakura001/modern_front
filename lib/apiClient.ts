c980c2672d7dd2a8114eb85c7402d0dcaef7cfe7
natuyukisakura <MoriRinMonro1@gmail.com>
2024-10-06 21:44:11 +0900

    [fix] backend URL

--- lib/apiClient.ts
+++ lib/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL, //環境変数で登録
+  baseURL: process.env.NEXT_PUBLIC_API_URL, //環境変数で登録
   headers: {
     "Content-type": "application/json", //形式の登録
   },
