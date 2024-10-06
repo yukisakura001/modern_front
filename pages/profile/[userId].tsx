cba1fe39faa89a0cec503161aad61d3697c97a62
natuyukisakura <MoriRinMonro1@gmail.com>
2024-10-06 19:57:05 +0900

    [6-58] プロフィール画面への投稿の出力

--- pages/profile/[userId].tsx
+++ pages/profile/[userId].tsx
@@ -1,20 +1,23 @@
 // 動的ルーティング
 
 import apiClient from "@/lib/apiClient";
-import { Profile } from "@/types";
+import { Profile, PostType } from "@/types";
 
 type Props = {
   profile: Profile;
+  posts: PostType[];
 };
 
 export const getServerSideProps = async (context: any) => {
   const { userId } = context.query;
   try {
-    const profileResponse = await apiClient.get(`/users/profile/${userId}`);
+    const profileResponse = await apiClient.get(`/users/profile/${userId}`); //特定のユーザーのプロフィールを取得
+    const postsResponse = await apiClient.get(`/posts/${userId}`); //特定のユーザーの投稿を取得
 
     return {
       props: {
         profile: profileResponse.data,
+        posts: postsResponse.data,
       },
     };
   } catch (e) {
@@ -24,7 +27,7 @@ export const getServerSideProps = async (context: any) => {
   }
 };
 
-const UserProfile = ({ profile }: Props) => {
+const UserProfile = ({ profile, posts }: Props) => {
   return (
     <div>
       <div className="container mx-auto px-4 py-8">
@@ -44,21 +47,28 @@ const UserProfile = ({ profile }: Props) => {
               </div>
             </div>
           </div>
-          <div className="bg-white shadow-md rounded p-4 mb-4">
-            <div className="mb-4">
-              <div className="flex items-center mb-2">
-                <img
-                  className="w-10 h-10 rounded-full mr-2"
-                  alt="User Avatar"
-                />
-                <div>
-                  <h2 className="font-semibold text-md">shincode</h2>
-                  <p className="text-gray-500 text-sm">2023/05/08</p>
+          {posts.map((post: PostType) => (
+            <div className="bg-white shadow-md rounded p-4 mb-4" key={post.id}>
+              <div className="mb-4">
+                <div className="flex items-center mb-2">
+                  <img
+                    className="w-10 h-10 rounded-full mr-2"
+                    alt="User Avatar"
+                    src={profile.profileImageUrl}
+                  />
+                  <div>
+                    <h2 className="font-semibold text-md">
+                      {post.author.username}
+                    </h2>
+                    <p className="text-gray-500 text-sm">
+                      {new Date(post.createdAt).toLocaleString()}
+                    </p>
+                  </div>
                 </div>
+                <p className="text-gray-700">{post.content}</p>
               </div>
-              <p className="text-gray-700">はじめての投稿です。</p>
             </div>
-          </div>
+          ))}
         </div>
       </div>
       ;
